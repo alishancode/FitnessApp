d10f264f658691f63e6af78be2048f500c293687
alishancode <alishan@byldd.com>
2024-11-09 14:03:01 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Md Alishan Ali on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by Md Alishan Ali on 09/11/24.
+//
+
+import Testing
+@testable import FitnessApp
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
